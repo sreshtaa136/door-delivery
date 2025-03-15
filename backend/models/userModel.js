588ebe67a7a9211f0007138c045771ec7534a380
sreshtaa136 <sreshtaa.t@gmail.com>
2025-03-15 12:11:24 +1100

    add user authentication

---
+++ backend/models/userModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    cartData: { type: Object, default: {} },
+  },
+  { minimize: false } // store empty objects
+);
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+export default userModel;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
 
 // app config
 const app = express();
@@ -15,6 +16,7 @@ app.use(cors());
 connectDB();
 
 // api endpoints
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
