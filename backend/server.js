4e709cc3180430f742e3268c12e94986542c4d04
sreshtaa136 <sreshtaa.t@gmail.com>
2025-03-16 23:27:54 +1100

    add cart functionality

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import "dotenv/config";
 import userRouter from "./routes/userRoute.js";
 import foodRouter from "./routes/foodRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // app config
 const app = express();
@@ -19,6 +20,7 @@ connectDB();
 // api endpoints
 app.use("/api/user", userRouter);
 app.use("/api/food", foodRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
