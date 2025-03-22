2758c5f5e15462fb74db9281d26f2176daef3a79
sreshtaa136 <sreshtaa.t@gmail.com>
2025-03-23 03:18:56 +1100

    fix payment issue

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // config variables
 const currency = "aud";
 const deliveryCharge = 3;
-const frontend_URL = "http://localhost:5173";
+const frontend_URL = process.env.REDIRECT_URL;
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
