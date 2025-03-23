21c2472fc78075169f9ec3abb39cd7980c627dcd
sreshtaa136 <sreshtaa.t@gmail.com>
2025-03-23 14:37:14 +1100

    switch to hash router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/">
+  <HashRouter basename="/">
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
