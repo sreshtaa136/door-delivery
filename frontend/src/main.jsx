f6bea879a5305decb9a48674ee7c4bdf8a184c45
sreshtaa136 <sreshtaa.t@gmail.com>
2025-03-23 14:26:10 +1100

    update base config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
